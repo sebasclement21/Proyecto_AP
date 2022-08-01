2219c07e3471e07626d7e05e2685078ed9e64295
Sebastian Clement <sebasclement.sc@gmail.com>
2022-08-01 19:07:19 -0300

    Experiencia laboral y educacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SocialsComponent } from './components/socials/socials.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AboutComponent } from './components/about/about.component';
+import { LaboralComponent } from './components/laboral/laboral.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SocialsComponent,
+    LogoComponent,
+    BannerComponent,
+    AboutComponent,
+    LaboralComponent
   ],
   imports: [
     BrowserModule,
